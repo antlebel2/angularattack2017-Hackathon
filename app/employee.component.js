78fcc8ea4280d9194e97e6b9a15b3cf3bd6cf285
Tyrel Tachibana <gramzanber@gmail.com>
2017-04-23 07:10:10 -0500

    Adding stuff for employee

--- app/employee.component.js
+++ app/employee.component.js
@@ -5,17 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var employeeApiPromiseService_1 = require("./shared/employeeApiPromiseService");
+var employeeDto_1 = require("./shared/employeeDto");
 var EmployeeComponent = (function () {
-    function EmployeeComponent() {
+    function EmployeeComponent(_employeePromiseService) {
+        this._employeePromiseService = _employeePromiseService;
     }
+    EmployeeComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this._personDetail = new employeeDto_1.EmployeeDto();
+        this._employeePromiseService.getService()
+            .then(function (personDetail) { return _this._personDetail = personDetail; })
+            .catch(function (error) { return console.log(error); });
+        this._personEmail = this._employeePromiseService.getQueryParam();
+    };
     return EmployeeComponent;
 }());
 EmployeeComponent = __decorate([
     core_1.Component({
-        templateUrl: 'app/employee.component.html'
-    })
+        templateUrl: 'app/employee.component.html',
+        providers: [employeeApiPromiseService_1.EmployeeApiPromiseService]
+    }),
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [employeeApiPromiseService_1.EmployeeApiPromiseService])
 ], EmployeeComponent);
 exports.EmployeeComponent = EmployeeComponent;
 //# sourceMappingURL=employee.component.js.map
--- app/employee.component.ts
+++ app/employee.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-import {Http} from '@angular/http';
+import { Component, Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeApiPromiseService } from './shared/employeeApiPromiseService';
+import { EmployeeDto } from './shared/employeeDto';
 
 @Component({
-    templateUrl: 'app/employee.component.html'
+    templateUrl: 'app/employee.component.html',
+    providers: [ EmployeeApiPromiseService ]
 })
+
+@Injectable()
 export class EmployeeComponent {
-    
+  private _personDetail:EmployeeDto;
+  private _personEmail:any;
+
+  constructor(private _employeePromiseService: EmployeeApiPromiseService){ }
+
+  ngOnInit() {
+      this._personDetail = new EmployeeDto();
+
+      this._employeePromiseService.getService()
+      .then(personDetail => this._personDetail = personDetail)
+      .catch(error => console.log(error));
+
+      this._personEmail = this._employeePromiseService.getQueryParam();
+      }
 }
