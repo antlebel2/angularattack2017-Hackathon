6d26b0a7fc47a9c39bec5734b44f13e34017a8de
Micah McKinnon <mmckinnon3@uco.edu>
2017-04-23 02:00:48 -0500

    added background image and other styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <div class="app-container">
-            <div class="component-container">
-                <router-outlet></router-outlet>
-            </div>
-            <div class="home-button-container">
-                <button class="home-button" [routerLink]="['/home/']">
-                <i class="fa fa-home"></i> Home</button>
-                <button class="home-button" [routerLink]="['/employee/']">Employee Page</button>
+        <div>
+            <div class="app-container">
+                <div class="component-container">
+                    <router-outlet></router-outlet>
+                </div>
+                <div class="home-button-container">
+                    <button class="home-button" [routerLink]="['/home/']">
+                    <i class="fa fa-home"></i> Home</button>
+                </div>
             </div>
         </div>
     `,
