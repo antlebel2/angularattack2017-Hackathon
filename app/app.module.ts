d77ddf85a7f71a741cb0d05fc616d3db737627cf
antlebel2 <antlebel@yahoo.com>
2017-04-22 22:14:52 -0500

    test2

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,12 +7,8 @@ import { AppComponent }  from './app.component';
 import { HomeComponent } from './home.component';
 import { EmployeeComponent } from './employee.component';
 import { EmployerComponent } from './employer.component';
-<<<<<<< HEAD
-import { EmployerBioComponent } from './employerBio.component';
 import { EmployerSocialComponent } from './employerSocial.component';
-=======
 import { EmployerWebComponent } from './employerWeb.component';
->>>>>>> 305c005142ec8dd736107e80d145e09f861ff9a6
 
 import { EmployerSelectedComponent } from './employerSelected.component';
 import { EmployeeSelectedComponent } from './employeeSelected.component';
@@ -27,12 +23,8 @@ import { EmployeeSelectedComponent } from './employeeSelected.component';
       { path: 'employer', component: EmployerComponent},
       { path: 'employerSelected', component: EmployerSelectedComponent },
       { path: 'employeeSelected', component: EmployeeSelectedComponent },
-<<<<<<< HEAD
-      { path: 'employer/bio', component: EmployerBioComponent },
       { path: 'employer/social', component: EmployerSocialComponent },
-=======
       { path: 'employer/webStatistics', component: EmployerWebComponent },
->>>>>>> 305c005142ec8dd736107e80d145e09f861ff9a6
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
     ]) 
   ],
@@ -43,12 +35,8 @@ import { EmployeeSelectedComponent } from './employeeSelected.component';
     EmployerComponent,
     EmployerSelectedComponent,
     EmployeeSelectedComponent,
-<<<<<<< HEAD
-    EmployerBioComponent,
-    EmployerSocialComponent
-=======
+    EmployerSocialComponent,
     EmployerWebComponent
->>>>>>> 305c005142ec8dd736107e80d145e09f861ff9a6
   ],
   bootstrap: [ 
     AppComponent
